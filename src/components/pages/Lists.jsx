3a84421952c97674409008c2435342f56cfd10a8
Ferdroid8080 <andresf@ferudinato.com>
2021-08-09 18:01:27 -0500

    [add] list for each board page

---
+++ src/components/pages/Lists.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Layout from '../Layout';
+import SearchToolbar from '../SearchToolbar';
+import TasksCards from '../Tasks/TasksCards';
+
+function Lists() {
+  return (
+    <Layout>
+      <div className="Lists">
+        <SearchToolbar />
+        <div className="Lists--Inner">
+          <div className="Lists--HeaderTitle">
+            <h4>¡Hola %Name%!</h4>
+            <div className="Lists--BoardsName">
+              <p>Tablero: %board_name%</p>
+              <p>(3 lists)</p>
+            </div>
+          </div>
+          <TasksCards list={[
+            { id: 101, title: 'To Do', number: 12, color: '#F49191' },
+            { id: 102, title: 'In Progress', number: 27, color: '#98CC90' },
+            { id: 104, title: 'Review', number: 1, color: '#cfcfcf' },
+            { id: 105, title: 'QA', number: 0, color: '#000000' },
+            { id: 103, title: 'Done', number: 7, color: '#AD86E0' },
+          ]} />
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Lists;
