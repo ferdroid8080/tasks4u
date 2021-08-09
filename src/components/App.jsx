ca1ab12339d6a0472a48e061d914e4023e543165
Ferdroid8080 <andresf@ferudinato.com>
2021-08-09 18:03:37 -0500

    linked list page and fixed pages path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import Boards from './pages/Boards';
-import Login from './pages/Login';
-import Onboarding from './pages/Onboarding';
+import Boards from './Pages/Boards';
+import Lists from './Pages/Lists';
+import Login from './Pages/Login';
+import Onboarding from './Pages/Onboarding';
 
 function App() {
 	let isLoggedIn = localStorage.getItem('trello_token') !== null;
@@ -11,6 +12,7 @@ function App() {
 		<Switch>
 			<Route exact path='/onboarding' component={Onboarding} />
 			<Route path='/auth' component={Login} />
+			<Route exact path='/boards/:id/lists' component={Lists} />
 			<Route path='/boards' component={Boards} />
 			<Redirect from='/' to={isLoggedIn ? '/boards' : '/onboarding'} />
 			<Route path='*' render={() => <h1>Not Found</h1>} />
