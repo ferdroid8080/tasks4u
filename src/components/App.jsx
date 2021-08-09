5e56f174e5db1ae1c422ac8ff93baf79b7e15faf
Ferdroid8080 <andresf@ferudinato.com>
2021-08-08 23:19:47 -0500

    linked boards component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Boards from './pages/Boards';
 import Login from './pages/Login';
 import Onboarding from './pages/Onboarding';
 
@@ -8,6 +9,7 @@ function App() {
 		<Switch>
 			<Route exact path='/' component={Onboarding} />
 			<Route path='/auth' component={Login} />
+			<Route path='/boards' component={Boards} />
 			<Route path='*' render={() => <h1>Not Found</h1>} />
 		</Switch>
 	);
