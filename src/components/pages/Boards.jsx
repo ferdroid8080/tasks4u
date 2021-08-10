1f70b1956b7a31a12dead4509b288f24eb6cf0bd
Ferdroid8080 <andresf@ferudinato.com>
2021-08-10 09:12:28 -0500

    wip: fetch boards from api

--- src/components/pages/Boards.jsx
+++ src/components/pages/Boards.jsx
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Layout from '../Layout';
 import SearchToolbar from '../SearchToolbar';
 
 function Boards() {
   let isLoggedIn = localStorage.getItem('trello_token') !== null;
+  let token = null;
 	const history = useHistory();
+  const [boards, setBoards] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchBoards = async () => {
+    const url = `${process.env.REACT_APP_TRELLO_API_URL}/members/me/boards?fields=name,url,memberships&key=${process.env.REACT_APP_TRELLO_API_KEY}&token=${token}`;
+    try {
+      let results = await fetch(url);
+      results = await results.json();
+      setBoards(results);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
     if (!isLoggedIn) {
       history.push('/auth');
+    } else {
+      token = localStorage.getItem('trello_token');
+      fetchBoards();
     }
   }, []);
 
@@ -21,16 +40,15 @@ function Boards() {
         <h2>Seleccione un tablero</h2>
         <p>Estos son sus tableros</p>
       </div>
-      <div className="Boards--Grid">
-        <div className="Boards--Item">
-          <p>Board Name</p>
-        </div>
-        <div className="Boards--Item">
-          <p>Board Name</p>
-        </div>
-        <div className="Boards--Item">
-          <p>Board Name</p>
-        </div>
+      <div className="Boards--Grid" style={{ gridTemplateColumns: loading ? '1fr' : '1fr 1fr' }}>
+        {loading ? <div className="Boards--Loading text-center">Cargando...</div> : null}
+        {
+          boards && boards.map(item => (
+            <div className="Boards--Item text-center" key={item.id}>
+              <p>{ item.name }</p>
+            </div>
+          ))
+        }
       </div>
     </div>
     </>
