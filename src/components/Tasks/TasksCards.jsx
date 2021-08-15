014aee8089b9352bf9c28fbef418a8a84e5b04fb
Ferdroid8080 <andresf@ferudinato.com>
2021-08-15 00:14:40 -0500

    [fix] changed one of the props for tasks card component

--- src/components/Tasks/TasksCards.jsx
+++ src/components/Tasks/TasksCards.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import TasksCardItem from './TasksCardItem';
 
 function TasksCards({ list }) {
-  
   return (
     <div className="Tasks--Cards">
       {
         list && list.map(task => (
-          <TasksCardItem key={task.id} number={task.number} title={task.title} color={task.color} />
+          <TasksCardItem key={task.id} number={task.number} title={task.name} color={task.color} />
         ))
       }
     </div>
